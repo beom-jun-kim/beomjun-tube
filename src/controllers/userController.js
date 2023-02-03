dbc3fc2f715084cfd9bd59c2c29c9ea8ec834fae
kimbeomjun <gimb52043@gmail.com>
2023-02-03 17:06:55 +0900

    user_part

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,12 @@
-export const join = (req, res) => res.send("Join!");
+export const getJoin = (req, res) => {
+  return res.render("join", { pageTitle: "Join" });
+};
+export const postJoin = (req, res) => {
+  console.log("안녕 나는 요청을 했어",req.body);
+  res.end();
+};
 export const edit = (req, res) => res.send("Edit USer");
 export const remove = (req, res) => res.send("Remove User");
 export const login = (req, res) => res.send("Login");
-export const logout =(req,res)=> res.send("logout");
-export const see = (req,res)=> res.send("see");
+export const logout = (req, res) => res.send("logout");
+export const see = (req, res) => res.send("see");
