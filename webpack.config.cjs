5f7b1a219ac4d47e0b3a32afb78b12ef6dea9845
kimbeomjun <gimb52043@gmail.com>
2023-02-23 14:07:59 +0900

    error

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -16,13 +16,24 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/, /* js코드를 */
+        test: /\.js$/ /* js코드를 */,
         use: {
-          loader: "babel-loader", /* babel-loader라는 loader로 가공 */
+          loader: "babel-loader" /* babel-loader로 가공 */,
           options: {
-            presets: [["@babel/preset-env", { targets: "defaults" }]], /* @babel/preset-env를 이용해서 */
+            presets: [
+              ["@babel/preset-env", { targets: "defaults" }],
+            ] /* 얘를 이용해서 */,
           },
         },
+      },
+      {
+        test: /\.scss$/,
+
+        // 작업 역순으로 선언 : webpack은 뒤에서부터 시작하기 때문
+        // 1. 일반 css로 변환
+        // 2. 그 코드를 css-loader한테 전달
+        // 3. 브라우저에 노출
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
