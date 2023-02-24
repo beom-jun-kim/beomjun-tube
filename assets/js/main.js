40315cc35205d44f4df5fd872789f8fa0b757800
kimbeomjun <gimb52043@gmail.com>
2023-02-24 17:34:14 +0900

    webpack_styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,13 +9,13 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/client/js/main.cjs":
-/*!********************************!*\
-  !*** ./src/client/js/main.cjs ***!
-  \********************************/
+/***/ "./src/client/js/main.js":
+/*!*******************************!*\
+  !*** ./src/client/js/main.js ***!
+  \*******************************/
 /***/ (() => {
 
-eval("throw new Error(\"Module parse failed: 'import' and 'export' may appear only with 'sourceType: module' (1:0)\\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\\n> import \\\"../scss/style.scss\\\";\\n| \\n| console.log(\\\"hi\\\");\");\n\n//# sourceURL=webpack://webtube/./src/client/js/main.cjs?");
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: Configuration should be an exported JavaScript object.\\n    at readConfigJS (C:\\\\workspace_beomjun\\\\beomjun-tube-master\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\files\\\\configuration.js:96:11)\\n    at readConfigJS.next (<anonymous>)\\n    at Function.<anonymous> (C:\\\\workspace_beomjun\\\\beomjun-tube-master\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\gensync-utils\\\\async.js:21:3)\\n    at Generator.next (<anonymous>)\\n    at step (C:\\\\workspace_beomjun\\\\beomjun-tube-master\\\\node_modules\\\\gensync\\\\index.js:261:32)\\n    at C:\\\\workspace_beomjun\\\\beomjun-tube-master\\\\node_modules\\\\gensync\\\\index.js:273:13\\n    at async.call.result.err.err (C:\\\\workspace_beomjun\\\\beomjun-tube-master\\\\node_modules\\\\gensync\\\\index.js:223:11)\");\n\n//# sourceURL=webpack://webtube/./src/client/js/main.js?");
 
 /***/ })
 
@@ -26,7 +26,7 @@ eval("throw new Error(\"Module parse failed: 'import' and 'export' may appear on
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
 /******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/client/js/main.cjs"]();
+/******/ 	__webpack_modules__["./src/client/js/main.js"]();
 /******/ 	
 /******/ })()
 ;
