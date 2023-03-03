7f06856eaa7079c4bf9eed59b3ec0e177270431f
kimbeomjun <gimb52043@gmail.com>
2023-03-03 12:09:06 +0900

    timeout

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -5,7 +5,11 @@ const volumeRange = document.getElementById("volume");
 const currenTime = document.getElementById("currenTime");
 const totalTime = document.getElementById("totalTime");
 const timeline = document.getElementById("timeline");
+const fullScreenBtn = document.getElementById("fullScreenBtn");
+const videoContainer = document.getElementById("videoContainer");
+const videoControls = document.getElementById("videoControls");
 
+let controlsMovementTimeout = null;
 let volumeValue = 0.5;
 video.volume = volumeValue;
 
@@ -66,7 +70,7 @@ const handleTimeUpdate = (e) => {
   timeline.value = Math.floor(video.currentTime);
 };
 
-// timeline
+// Timeline
 const handletimelineChange = (e) => {
   const {
     target: { value },
@@ -74,9 +78,44 @@ const handletimelineChange = (e) => {
   video.currentTime = value;
 };
 
+// Full screen
+const handleFullscreen = (e) => {
+  const fullscreen = document.fullscreenElement;
+  if (fullscreen) {
+    // exit하고 innerText를 enter로 바꿈
+    // document.fullscreenElement는 그냥 전체화면인지 아닌지 알려주는 요소일 뿐이다
+    document.exitFullscreen();
+    fullScreenBtn.innerText = "Enter Full Screen";
+  } else {
+    videoContainer.requestFullscreen();
+    fullScreenBtn.innerText = "Exit Full Screen";
+  }
+};
+
+// Mouse Move
+
+// 커서를 움직일시 visible이벤트가 실행되고 멈추면 
+// if문에 있는 controlsMovementTimeout가 실행된다
+const handleMouseMove = (e) => {
+  
+  // 비디오 ele 내에서 커서를 움직일때
+  if(controlsMovementTimeout) {
+    
+    // clearTimeout : 함수가 항상 한번만 실행하도록
+    clearTimeout(controlsMovementTimeout);
+    controlsMovementTimeout = null;
+  }
+  videoControls.classList.add("visible");
+  controlsMovementTimeout = setTimeout(hideControls, 3000);
+};
+
+const hideControls = () => videoControls.classList.remove("visible");
+
 playBtn.addEventListener("click", handlePlay);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange);
 video.addEventListener("loadedmetadata", handleLoadedMetadata);
 video.addEventListener("timeupdate", handleTimeUpdate);
 timeline.addEventListener("input", handletimelineChange);
+fullScreenBtn.addEventListener("click", handleFullscreen);
+video.addEventListener("mousemove", handleMouseMove);
