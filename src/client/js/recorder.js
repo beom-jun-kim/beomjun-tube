b34f9331411b94ea3823a7f3ea4e284825f11a53
kimbeomjun <84590988+beom-jun-kim@users.noreply.github.com>
2023-03-08 12:56:18 +0900

    Update recorder.js

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -14,7 +14,6 @@ const handleDownload = async () => {
   const ffmpeg = createFFmpeg({ log: true });
 
   // 소프트웨어가 무거울 수 있기에 기다려줘야함
-  // 사용자가 소프트웨어를 사용할 것이기 때문
   await ffmpeg.load();
 
   // writeFile : ffmpeg 가상의 어떤 곳에 파일 생성
@@ -26,8 +25,21 @@ const handleDownload = async () => {
   // -r 60 : 초당 60프레임으로 인코딩
   await ffmpeg.run("-i", "rec.webm", "-r", "60", "output.mp4");
 
+  // -ss: 특정시간대로 이동
+  // -frames:v : 첫프레임의 스크린샷
+  await ffmpeg.run(
+    "-i",
+    "rec.webm",
+    "-ss",
+    "00:00:01",
+    "-frames:v",
+    "1",
+    "thumbnail.jpg"
+  );
+
   // readFile : 데이터 읽기
   const mp4file = ffmpeg.FS("readFile", "output.mp4");
+  const thumbFile = ffmpeg.FS("readFile", "thumbnail.jpg");
 
   // Uint8Array(양의 정수 8비트 배열) , buffer 배열 반환 (데이터)
   // 이 배열로부터 blob을 만들어내야한다
@@ -36,15 +48,34 @@ const handleDownload = async () => {
   // buffer는 arrayBuffer 반환 , arrayBuffer는 raw binary data를 나타내는 obj
   // new Blob()는 배열안에 배열들을 받을 수 있다
   const mp4Blob = new Blob([mp4file.buffer], { type: "video/mp4" });
+  const thumbBlob = new Blob([thumbFile.buffer], { type: "image/jpg" });
 
-  // 브라우저에서 해당 파일 url을 만들어준다
+  // createObjectURL() : 브라우저에서 해당 파일 url을 만들어준다
   const mp4Url = URL.createObjectURL(mp4Blob);
+  const thumbUrl = URL.createObjectURL(thumbBlob);
 
   const a = document.createElement("a");
   a.herf = mp4Url;
   a.download = "나의녹화파일.mp4";
   document.body.appendChild(a);
   a.click();
+
+  const thumbImg = document.createElement("a");
+  thumbImg.herf = thumbUrl;
+  thumbImg.download = "썸네일.jpg";
+  document.body.appendChild(thumbImg);
+  thumbImg.click();
+
+  // 파일삭제
+  ffmpeg.FS("unlink","output.mp4");
+  ffmpeg.FS("unlink","thumbnail.jpg");
+
+  // url 삭제
+  // revokeObjectURL() : 객체 URL을 더는 쓸 일이 없을 때 사용.
+  ffmpeg.FS("unlink","rec.webm");
+  URL.revokeObjectURL(thumbUrl);
+  URL.revokeObjectURL(mp4Url);
+  URL.revokeObjectURL(videoFile);
 };
 
 const handleStop = () => {
