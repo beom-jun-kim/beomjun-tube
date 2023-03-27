e0d2ffeae40fe284b7d2ba9b9e5b1d952b5cd273
kimbeomjun <gimb52043@gmail.com>
2023-03-27 09:50:25 +0900

    dfdd

--- src/init.js
+++ src/init.js
@@ -1,9 +1,10 @@
-import "dotenv/config";
 import "./db.js";
 import "./models/video.js";
 import "./models/user.js";
 import "./models/comment.js"
 import app from "./server.js";
+// import "dotenv/config";
+require('dotenv').config();
 
 const PORT = 4000;
 
