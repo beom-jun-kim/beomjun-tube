0363e6518d332a2a218f374af9efdf37d929259c
kimbeomjun <gimb52043@gmail.com>
2023-03-27 16:00:13 +0900

    sadf

--- src/init.js
+++ src/init.js
@@ -1,9 +1,16 @@
+import esbuild from "esbuild"
+import Dotenv from 'dotenv-esbuild';
 import "dotenv/config";
 import "./db.js";
 import "./models/video.js";
 import "./models/user.js";
 import "./models/comment.js"
 import app from "./server.js";
+
+esbuild.build({
+  
+  plugins : new Dotenv()
+})
 
 const PORT = 4000;
 
