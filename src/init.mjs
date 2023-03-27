52dc33a4d5dd7479560bfdfea6e3fb313e759262
kimbeomjun <gimb52043@gmail.com>
2023-03-27 16:06:29 +0900

    asdf

---
+++ src/init.mjs
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import "./db.js";
+import "./models/video.js";
+import "./models/user.js";
+import "./models/comment.js"
+import app from "./server.js";
+
+const PORT = 4000;
+
+const handleListening = () => {console.log(`hi http://localhost:${PORT}`)};
+app.listen(PORT, handleListening);
