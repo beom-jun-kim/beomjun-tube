e7f7a905040020f4dbc2670eca39a08844934a30
kimbeomjun <gimb52043@gmail.com>
2023-03-27 17:07:26 +0900

    sadf

--- src/init.js
+++ src/init.js
@@ -1,9 +1,9 @@
 import dotenv from "dotenv";
 dotenv.config();
-import db from "./db.mjs";
-import Video from "./models/video.mjs";
-import User from "./models/user.mjs";
-import Comment from "./models/comment.mjs";
+import { db } from "./db.mjs";
+import { Video } from "./models/video.mjs";
+import { User } from "./models/user.mjs";
+import { Comment } from "./models/comment.mjs";
 import app from "./server.mjs";
 
 const PORT = 4000;
