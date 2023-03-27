4301f1a7c6ba71908563cb0b24f8966907a8fe88
kimbeomjun <gimb52043@gmail.com>
2023-03-27 17:08:42 +0900

    asdf

--- src/init.js
+++ src/init.js
@@ -1,18 +1,13 @@
 import dotenv from "dotenv";
 dotenv.config();
-import { db } from "./db.mjs";
-import { Video } from "./models/video.mjs";
-import { User } from "./models/user.mjs";
-import { Comment } from "./models/comment.mjs";
+import db from "./db.mjs";
+import Video from "./models/video.mjs";
+import User from "./models/user.mjs";
+import Comment from "./models/comment.mjs";
 import app from "./server.mjs";
-
 const PORT = 4000;
 
 const handleListening = () => {
   console.log(`hi http://localhost:${PORT}`);
 };
 app.listen(PORT, handleListening);
-Video.listen(PORT, handleListening);
-User.listen(PORT, handleListening);
-Comment.listen(PORT, handleListening);
-db.listen(PORT, handleListening);
