6f12eb8abfd0781b7711394204a786a65c920c08
kimbeomjun <gimb52043@gmail.com>
2023-03-28 11:37:18 +0900

    asdf

--- src/init.js
+++ src/init.js
@@ -1,9 +1,9 @@
-import "./dotenv.mjs";
-import "./db.mjs";
-import "./models/video.mjs";
-import "./models/user.mjs";
-import "./models/comment.mjs";
-import app from "./server.mjs";
+import "dotenv/config";
+import "./db.js";
+import "./models/video.js";
+import "./models/user.js";
+import "./models/comment.js";
+import app from "./server.js";
 
 // 내컴퓨터에서는 4000 , heroku에서 실행될때는 port변수
 const PORT = process.env.PORT || 4000;
